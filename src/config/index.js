676777c949539f082e9b6bc4f215a32672d4f7db
Alberto Palumbo <palumbo.alb@gmail.com>
2017-08-04 01:04:20 -0400

    fixed warning critical dependency

--- src/config/index.js
+++ src/config/index.js
@@ -2,14 +2,4 @@ var config = {
   axios: {},
 };
 
-if (process.env.NODE_ENV === 'test') {
-  const path = require('path');
-  const lib = path.join(
-    path.dirname(require.resolve('axios')),
-    'lib/adapters/http',
-  );
-  const http = require(lib);
-  config.axios.adapter = http;
-}
-
 export { config };
